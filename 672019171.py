a9e130c349e32f82c30f750fac913d5c7cfdb661
Ivanandika21 <54906064+ivanandika21@users.noreply.github.com>
2021-02-21 09:13:56 +0700

    Add files via upload

---
+++ 672019171.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+graf = {
+    'A' : ['B', 'C', 'D'],
+    'B' : ['E', 'F', 'G', 'H'],
+    'F' : ['O', 'P', 'Q'],
+    'G' : ['R', 'S'],
+    'C' : ['I', 'J', 'K'],
+    'K' : ['T'],
+    'D' : ['L', 'M', 'N'],
+    'M' : ['U'],
+}
+
+initVar = 'A'
+
+def tampung(inittial, dari_graf):
+    sudah = []
+    sedang = [inittial]
+
+    while sedang:
+        nodeSaatIni = sedang.pop(0)
+        if nodeSaatIni not in sudah:
+            sudah.append(nodeSaatIni)
+        if nodeSaatIni in dari_graf:
+            for anak in dari_graf[nodeSaatIni]:
+                sedang.append(anak)
+    return sudah
+
+def mainnn():
+    i = 0
+print("Ivan Andika Surya 672019171")
+
+if __name__ == "__main__":
+    print('Vertexlist : ', end=(' '))
+    print(tampung(initVar, graf))
+
+my_array = np.array(tampung(initVar, graf))
+tujuan = input('\nMasukkan node tujuan : ')
+
+for i in range(1, len(tampung(initVar, graf))+1):
+    print('urutan ke', i, end=('\t>> '))
+
+    if tujuan in my_array[i-1]:
+        print('Pencarian berhenti, node saat ini adalah tujuan yaitu', end=(' '))
+        print(my_array[i-1])
+        break
+    print(my_array[i-1])
+
+if tujuan not in my_array[i-1]:
+        print('Pencarian berhenti, tujuan tidak ditemukan!')
+
+print('\n')
+
